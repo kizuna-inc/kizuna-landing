df0560d6ebbe9ed9a7fff59cd5aeb1e003ec0d7a
Suphakit P <me@suphakit.net>
2024-09-18 14:26:34 +0700

    feat<section>: added about section

--- src/Components/Sections/About.tsx
+++ src/Components/Sections/About.tsx
@@ -12,9 +12,34 @@ export const About = (child: childInterface) => {
         <p className="font-bold text-3xl mb-4 text-whitesmoke underline">
           About
         </p>
-        <p className="text-xl text-whitesmoke px-4">
-          Do the thing we need with passion.
-        </p>
+        <div className="w-full flex flex-col gap-4">
+          <p className="text-xl text-whitesmoke px-4">
+            We are a team of IT students who want to learn anything even if it's
+            hard or not.
+          </p>
+          <p className="text-xl text-whitesmoke px-4">
+            At this moment, we really want to tried on Game Developing and we
+            are joining some GameJam on{" "}
+            <a
+              href="https://itch.io"
+              target="_blank"
+              className="underline hover:opacity-60 hover:no-underline"
+            >
+              Itch.io
+            </a>
+          </p>
+          <p className="text-xl text-whitesmoke px-4">
+            If you want to give us some advices or want to take with us, it's
+            okay to contact us on{" "}
+            <a
+              href="mailto:caffe@slpy.one"
+              target="_blank"
+              className="underline hover:opacity-60 hover:no-underline"
+            >
+              E-Mail
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
