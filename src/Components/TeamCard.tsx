c1f0da4ba29f2694e5234b4f0429671808a4d900
Suphakit P <me@suphakit.net>
2024-09-18 14:28:12 +0700

    feat<components>: change team card style

--- src/Components/TeamCard.tsx
+++ src/Components/TeamCard.tsx
@@ -8,18 +8,19 @@ export const TeamCard = (child: childInterface) => {
   return (
     <a
       href={`https://github.com/${child.team.github}`}
-      className="w-[18rem] rounded-xl bg-whitesmoke text-jet hover:scale-95 hover:opacity-80"
+      className="w-[18rem] rounded-xl text-jet hover:scale-95"
       target="_blank"
     >
       <div
-        className="!bg-center !bg-cover !bg-no-repeat w-full h-[14rem] rounded-t-xl"
+        className="!bg-center !bg-cover !bg-no-repeat w-full h-[18rem] rounded-xl group"
         style={{
           background: `url(https://avatars.githubusercontent.com/${child.team.github})`,
         }}
-      ></div>
-      <div className="mt-2 flex flex-col gap-2 px-4 py-2 pb-4">
-        <h1 className="font-semibold underline">{child.team.name}</h1>
-        <p>&nbsp;&nbsp;{child.team.desp}</p>
+      >
+        <div className="h-full w-full flex flex-col text-center justify-end items-center text-whitesmoke pb-4 opacity-0 group-hover:h-full group-hover:bg-overlay group-hover:opacity-100">
+          <h1 className="font-semibold underline">{child.team.name}</h1>
+          <p>&nbsp;&nbsp;{child.team.desp}</p>
+        </div>
       </div>
     </a>
   );
