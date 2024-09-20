53a06d80d35fe049711126dcddf5ef17ea522286
Suphakit P <me@suphakit.net>
2024-09-20 10:23:45 +0700

    chore<section>: added sub section in works section

--- src/Components/Sections/Works.tsx
+++ src/Components/Sections/Works.tsx
@@ -1,4 +1,4 @@
-import { WorksData } from "../../Modules/Works";
+import { GameJam, OtherWorks } from "../../Modules/Works";
 import { WorkCard } from "../WorksCard";
 
 interface childInterface {
@@ -18,10 +18,23 @@ export const Works = (child: childInterface) => {
         <p className="text-xl text-whitesmoke px-4">
           This section gonna show you the works we have done before!
         </p>
+
+        <p className="text-2xl text-whitesmoke font-semibold underline p-4 mt-2">
+          GameJam's Works that we have joined. 
+        </p>
         <div className="md:w-[80%] w-full py-4 flex flex-row flex-wrap justify-center items-start gap-4 mx-auto">
-          {WorksData.map((data) => {
+          {GameJam.map((data) => {
             return <WorkCard works={data} />;
           })}
+        </div>
+
+        <p className={`${OtherWorks.length > 0 ? '' : 'hidden'} text-2xl text-whitesmoke font-semibold underline p-4 mt-2`}>
+          Other works
+        </p>
+        <div className={`${OtherWorks.length > 0 ? '' : 'hidden'} md:w-[80%] w-full py-4 flex flex-row flex-wrap justify-center items-start gap-4 mx-auto`}>
+          {
+            OtherWorks.length > 0 ? OtherWorks.map((data) => { return <WorkCard works={data} /> }) : ''
+          }
         </div>
       </div>
     </div>
--- src/Modules/Works.ts
+++ src/Modules/Works.ts
@@ -5,7 +5,7 @@ export interface WorksInterface {
   links: string;
 }
 
-export const WorksData: WorksInterface[] = [
+export const GameJam: WorksInterface[] = [
   {
     name: "Unexpected ISEKAI",
     desp: "TableTop Role Playing Game based on a Isekai story set.",
@@ -13,3 +13,9 @@ export const WorksData: WorksInterface[] = [
     links: "https://kizunainc.itch.io/unexpected-isekai",
   },
 ];
+
+// This one gonna use same Interface as the above one :
+
+export const OtherWorks: WorksInterface[] = [
+  
+]
